a0e6ffaf7ecf574886de40ad9485cde92c46f61f
Yuchen Song <sycamore361@hotmail.com>
2024-02-22 01:17:00 -0800

    added weld joint

--- Test/with_rope.py
+++ Test/with_rope.py
@@ -1,5 +1,5 @@
 import pygame
-from Box2D import (b2World, b2PolygonShape, b2_dynamicBody, b2RopeJointDef)
+from Box2D import (b2World, b2PolygonShape, b2_dynamicBody, b2DistanceJointDef, b2WeldJointDef)
 import random
 
 # Pygame setup
@@ -24,22 +24,35 @@ def create_block(x, y, width, height):
 blocks = [create_block(random.randint(0, width-50), 600, 50, 50) for _ in range(20)]
 # Add a second row of blocks starting higher
 blocks.extend([create_block(random.randint(0, width-50), 800, 50, 50) for _ in range(20)])
+
 # Add a floor
 ground = world.CreateStaticBody(position=(0, 0))
 ground.CreateEdgeFixture(vertices=[(0, 0), (width/ppm, 0)], density=1, friction=0.3)
 
-ropes = []
+distance_joints = []
 for i in range(3):
     block1 = random.choice(blocks)
     block2 = random.choice([block for block in blocks if block != block1])  # Ensure block2 is different from block1
-    rope_joint = b2RopeJointDef()
-    rope_joint.bodyA = block1
-    rope_joint.bodyB = block2
-    rope_joint.localAnchorA = (0, 0)
-    rope_joint.localAnchorB = (0, 0)
-    rope_joint.maxLength = 2.0
-    world.CreateJoint(rope_joint)
-    ropes.append(rope_joint)
+    distance_joint = b2DistanceJointDef()
+    distance_joint.bodyA = block1
+    distance_joint.bodyB = block2
+    distance_joint.localAnchorA = (0, 0)
+    distance_joint.localAnchorB = (0, 0)
+    distance_joint.length = 4.0
+    world.CreateJoint(distance_joint)
+    distance_joints.append(distance_joint)
+
+weld_joints = []
+for i in range(3):
+    block1 = random.choice(blocks)
+    block2 = random.choice([block for block in blocks if block != block1])  # Ensure block2 is different from block1
+    weld_joint = b2WeldJointDef()
+    weld_joint.bodyA = block1
+    weld_joint.bodyB = block2
+    weld_joint.localAnchorA = (block1.fixtures[0].shape.vertices[0][0], block1.fixtures[0].shape.vertices[0][1])
+    weld_joint.localAnchorB = (block2.fixtures[0].shape.vertices[1][0], block2.fixtures[0].shape.vertices[1][1])
+    world.CreateJoint(weld_joint)
+    weld_joints.append(weld_joint)
 
 def draw_block(body, color):
     for fixture in body.fixtures:
@@ -48,14 +61,24 @@ def draw_block(body, color):
         vertices = [(v[0], height - v[1]) for v in vertices]
         pygame.draw.polygon(screen, color, vertices)
 
-def draw_rope(rope_joint):
-    bodyA = rope_joint.bodyA
-    bodyB = rope_joint.bodyB
-    posA = bodyA.transform * rope_joint.localAnchorA
-    posB = bodyB.transform * rope_joint.localAnchorB
+def draw_distance_joint(distance_joint):
+    bodyA = distance_joint.bodyA
+    bodyB = distance_joint.bodyB
+    posA = bodyA.transform * distance_joint.localAnchorA
+    posB = bodyB.transform * distance_joint.localAnchorB
     posA = (posA[0] * ppm, height - posA[1] * ppm)
     posB = (posB[0] * ppm, height - posB[1] * ppm)
-    pygame.draw.line(screen, (0, 0, 0), posA, posB, 2)
+    pygame.draw.line(screen, (255, 0, 0), posA, posB, 4)
+
+def draw_weld_joint(weld_joint):
+    bodyA = weld_joint.bodyA
+    bodyB = weld_joint.bodyB
+    posA = bodyA.transform * weld_joint.localAnchorA
+    posB = bodyB.transform * weld_joint.localAnchorB
+    posA = (posA[0] * ppm, height - posA[1] * ppm)
+    posB = (posB[0] * ppm, height - posB[1] * ppm)
+    pygame.draw.circle(screen, (0, 0, 255), posA, 6)
+    pygame.draw.circle(screen, (0, 0, 255), posB, 6)
 
 running = True
 while running:
@@ -72,8 +95,11 @@ while running:
     for i, block in enumerate(blocks):
         draw_block(block, BLOCK_COLORS[i % len(BLOCK_COLORS)])
 
-    for rope in ropes:
-        draw_rope(rope)
+    for distance_joint in distance_joints:
+        draw_distance_joint(distance_joint)
+
+    for weld_joint in weld_joints:
+        draw_weld_joint(weld_joint)
 
     pygame.display.flip()
     clock.tick(60)
